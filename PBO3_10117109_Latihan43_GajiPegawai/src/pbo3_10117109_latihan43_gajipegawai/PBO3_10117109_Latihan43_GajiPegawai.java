d101648408a96d271b86f4f48cbf6bf7c4da5f7c
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-10-22 21:23:56 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan43_GajiPegawai/src/pbo3_10117109_latihan43_gajipegawai/PBO3_10117109_Latihan43_GajiPegawai.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan43_gajipegawai;
+
+/**
+ * Nama  : Stefanus Saputra
+ * NIM   : 10117109
+ * Kelas : PBO3
+ * Deskripsi Program  : Program ini berisi program untuk menampilkan 
+ * hasil saldo yang sudah melakukan penarikan uang
+ */
+public class PBO3_10117109_Latihan43_GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("### Data Gaji Karyawan PT.KAKATU ###");
+        System.out.println("------------------------------------");
+        GajiPegawai karyawan1 = new GajiPegawai();
+        karyawan1.setNama("Stefanus Saputra");
+        karyawan1.setAlamat("Jalan Tubagus Ismail VI no 33");
+        karyawan1.setUangTransport(250000);
+        karyawan1.setUangTunjangan(300000);
+        karyawan1.setGajiPokok(4500000);
+        karyawan1.setTotalGaji(karyawan1.totalGaji(karyawan1.getUangTunjangan()
+                ,karyawan1.getUangTransport(),karyawan1.getGajiPokok()));
+        karyawan1.tampilData(karyawan1.getNama(),karyawan1.getAlamat(),
+                karyawan1.getUangTunjangan(),karyawan1.getUangTransport()
+                ,karyawan1.getGajiPokok(),karyawan1.getTotalGaji());
+    }
+    
+}
